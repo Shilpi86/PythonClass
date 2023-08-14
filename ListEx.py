3237196eb84268fe83572a5964d5927ac407c279
https://github.com/Shilpi86/PythonClass.git <shilpithaj@gmail.com>
2023-08-14 17:11:53 -0500

    changes in day4 th class

---
+++ ListEx.py
@@ -0,0 +1,18 @@
+a1 = [12, 45, 566,'wassup',90,'hi',45] #stores the values in 0,1,2,3... index position
+print(a1[:5])
+print(a1[2:5])
+print(a1[0:6])
+print(a1)
+a1.append(47394) #append the data to list
+print(a1)
+a1.insert(2,'list examples') #insert the value at specific position
+print(a1)
+a1[6]='same' #replacing the value at specific position
+print(a1)
+a1.remove(45) #removes the specific value from the list
+print(a1)
+a1.pop() #by default removes the last value
+print(a1)
+a1.pop(5) #removes the value based on index position
+print(a1)
+print(a1)
