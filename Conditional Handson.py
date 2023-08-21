2aae504e32c19181fa8a7b3f1387ad9ceaaab2b8
https://github.com/Shilpi86/PythonClass.git <shilpithaj@gmail.com>
2023-08-21 11:15:39 -0500

    day 5

---
+++ Conditional Handson.py
@@ -0,0 +1,2 @@
+#Write a program that reads the students marks as input and prints PASS or FAIL.
+# If the student score more than 50, print PASS.
---
+++ ConditionalEx.py
@@ -0,0 +1,19 @@
+a=30
+b=40
+if(a>=b):
+    print("Smaller Value")
+else:
+    print("condition not fulfilled")
+
+
+a=50
+b=50
+
+if a>b:
+    print("Greater Value")
+elif a<b:
+    print("Lesser Value")
+elif a==b:
+    print("Equals")
+else:
+    print("Not Valid")
