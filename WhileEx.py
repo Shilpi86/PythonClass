0f725369befc4269a8ab6987d79925e377385561
https://github.com/Shilpi86/PythonClass.git <shilpithaj@gmail.com>
2023-08-21 14:33:42 -0500

    Day6

--- WhileEx.py
+++ WhileEx.py
@@ -1,5 +1,5 @@
+#when you are executing the program in loop when you don't have a fixed amount of values in db
 a = 0
 while a<=15:
     print(a)
     a=a+2
-#when you are executing the program in loop when you don't have a fixed amount of values in db
