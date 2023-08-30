cc82e1fc038653b4e440c1c205f233ef18532a4e
https://github.com/Shilpi86/PythonClass.git <shilpithaj@gmail.com>
2023-08-30 11:54:04 -0500

    Day6

--- LoopingHandson.py
+++ LoopingHandson.py
@@ -42,17 +42,64 @@ print(" # " * m)
 for i in range(1,n+1):
     print(" # " * m)
 
-#Hollow Rectangle
+#Hollow Rectangle:
 
+print("This is the Hollow rectangle")
+m = int(input("Enter the number of rows: "))
+n = int(input("Enter the number of columns: "))
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if (i == 1 or i == n or
+                j == 1 or j == m):
+            print("#", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
 
 
 #4. Write Program that reads a number M and N and prints N numbers after M.
 
-M = int(input("Enter the value" ))
-N = int(input("Enter the value" ))
-
+m = int(input("the values of m: "))
+n = int(input("the values of n: "))
+print(m,n)
+for i in range(m, M+n):
+    print(i)
 
 #5. Given an integer N , write a program which reads N inputs and prints the sum of the given input number.
 
 
+
+
+
 #6. Write a program that reads a number N and prints two Right Angled Triangles of N Rows , using numbers starting from 1.
+
+N = int(input("Enter the value of N: "))
+
+# Right-angled Triangle 1
+for i in range(1, N + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+# Right-angled Triangle 2
+for i in range(1, N + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+
+# 7. Write a program that reads a number N and prints the sum of squares of N numbers starting from 1.
+#Explanation: if the N=4
+#The 4 numbers starting from 4 are 1,2,3,4
+#Squares of those numbers are 1,4,9,16 Sum of squares are 1+4+9+16=30 Output should be 30.
+
+n = int(input("Enter a number: "))
+sum_of_squares = 0
+
+for i in range(1, n + 1):
+    print(i)
+    squared_value = i ** 2
+    print(str(i) + "^2 = " + str(squared_value))
+    sum_of_squares += squared_value
+
+print("Sum of squares:", sum_of_squares)
