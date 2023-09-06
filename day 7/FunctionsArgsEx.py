fa953a7269ee474ab9fc11055d2859421f99a795
https://github.com/Shilpi86/PythonClass.git <shilpithaj@gmail.com>
2023-09-06 12:23:18 -0500

    changes

--- day 7/FunctionsArgsEx.py
+++ day 7/FunctionsArgsEx.py
@@ -3,13 +3,13 @@ def add(a,b): #passing variables as parameters
     print(a+b)
 
 def sub(a,b):
-    print(a+b)
+    print(a-b)
 
 def mul(a,b):
-    print(a+b)
+    print(a*b)
 
 def div(a,b):
-    print(a+b)
+    print(a/b)
 
 add(10,30) #passing the values(arguments) at teh time of calling the functions.
 mul(100,5)
---
+++ day 8/ClassOOPsEx.py
@@ -0,0 +1,14 @@
+class Sample: #creating a class
+    a = 20
+    b = 80
+# allocating variables anf functions( properties) to a class
+    def m1(self):
+        print(m1)
+
+
+#creating an object for class
+# identifying my entire class properties info under the name 'obj'
+
+obj = Sample()
+print(obj.a)
+print(obj.b)
