f36b8913a0c65cd98dccb82e424f68d77cf12c12
https://github.com/Shilpi86/PythonClass.git <shilpithaj@gmail.com>
2023-09-22 13:52:48 -0500

    day19

--- Handson/DatabaseHandson.py
+++ Handson/DatabaseHandson.py
@@ -1,48 +1,22 @@
 from mysql import connector
-
-class Table:
-    def __init__(self, host, user, password, database):
-        self.myDbConnection = connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database
-        )
-        self.c1 = self.myDbConnection.cursor()
-
-    #def create_table(self):
-        #self.c1.execute("CREATE TABLE School (name VARCHAR(20), class INT, grades INT)")
-        #self.myDbConnection.commit()
+class Table: #within the class not outside of the class.
+    global myDbConnection
+    myDbconnection=connector.connect (host="Localhost", user= "root", password= "Amyra262017", database= "pythonclass")
+    global c1 #when you want to access the variable outside of the function (globally) this should be used
+    c1=myDbconnection.cursor()
+    def create(self): # create is DDL operation not DRL , you do not need commit or close function
+        c1. execute("create Table BUS (name varchar (20), class int, school varchar(20))")
 
     def insert(self):
-        self.c1.execute("INSERT INTO School VALUES ('wik', 7, 90)")
-        self.c1.execute("INSERT INTO School VALUES ('Sam', 5, 92)")
-        self.myDbConnection.commit()
-        print(f"{self.c1}")
-
-    def update(self):
-        self.c1.execute("update stud set marks=92 where name='wik'")
-        self.myDbConnection.commit()
-
-
-    def alter(self):
-         self.c1.execute("alter table stud add age")
-         self.myDbConnection.close()
+        c1.insert("insert into BUS values ('wik', 7, 'nelson')")
+        c1.insert("insert into BUS values('Sam', 8, 'carriage')")
+        myDbConnection.commit()
+        myDbConnection.close()
 
     def select(self):
-        self.c1.excecute("select * from stud")
-        result = self.c1.fetchall()
+        c1.excecute("select * from BUS")
+        result=fetchall()
         print(result)
-        self.myDbConnection.close()
-    def close_connection(self):
-        self.myDbConnection.close()
 
-# Initialize the Table class with your database credentials
-db = Table(host='localhost', user='root', password='Amyra262017', database='pythonclass') # Create the "School" table
-#db.create_table()
-
-# Insert data into the "School" table
-db.insert()
-
-# Close the database connection when done
-db.close_connection()
+obj1 = Table()
+obj1.create ()
--- day 19/DbEx3.py
+++ day 19/DbEx3.py
@@ -1,11 +1,14 @@
+
+#fetching data from the dabase to the pycharm
 from mysql import connector
 myDbConnection = connector.connect(host='localhost', user='root', password='Amyra262017', database="pythonclass")
 
 c1 = myDbConnection.cursor()
-c1.execute("select * from Stationary where item_quantity=10")
+c1.execute("select * from Stationary ")
 
-result = c1.fetchall()  # or fetchone or fetchmany(size=2)
-
+#result = c1.fetchall()  # or fetchone or fetchmany(size=2)
+#result = c1.fetchmany(size=2)
+result = c1.fetchone()
 for i in result:
     print(i)
 myDbConnection.close()
