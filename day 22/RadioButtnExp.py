bd29778ccec702cbd6f508f254ff15b7a5ba6a32
https://github.com/Shilpi86/PythonClass.git <shilpithaj@gmail.com>
2023-09-28 12:02:55 -0500

    day22

--- day 22/RadioButtnExp.py
+++ day 22/RadioButtnExp.py
@@ -29,28 +29,28 @@ root.mainloop()
 # OR
 
 
-from tkinter import *
-from tkinter import messagebox
+# rom tkinter import *
+# from tkinter import messagebox
 
-root = Tk()
-root.geometry('250x250')
+# root = Tk()
+# root.geometry('250x250')
 
-v1=IntVar()
+# v1=IntVar()
 
-def m1():
-    select = "Selected option is "+str(v1.get()) #v1.get()
-    lb.config(text=select)
+# def m1():
+    # select = "Selected option is "+str(v1.get()) #v1.get()
+    # lb.config(text=select)
 
-r1 = Radiobutton(root,text="Mango", variable=v1, value=1, command = m1) #value='mango'
-r2 = Radiobutton(root,text="Apple", variable=v1, value=2, command = m1)
-r3 = Radiobutton(root,text="Orange", variable=v1, value=3, command = m1)
+# r1 = Radiobutton(root,text="Mango", variable=v1, value=1, command = m1) #value='mango'
+# r2 = Radiobutton(root,text="Apple", variable=v1, value=2, command = m1)
+# r3 = Radiobutton(root,text="Orange", variable=v1, value=3, command = m1)
 
-r1.pack()
-r2.pack()
-r3.pack()
+# r1.pack()
+# r2.pack()
+# r3.pack()
 
 
-lb = Label(root)
-lb.pack()
+# lb = Label(root)
+# lb.pack()
 
-root.mainloop()
+# root.mainloop()
